b73a7790f2f11876246c6b9f097a0931a4c90389
zozo17-ux <quispezoe7@gmail.com>
2025-04-22 08:08:39 -0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Inicializa el carrusel para asegurarse de que funciona correctamente
+    let myCarousel = new bootstrap.Carousel(document.querySelector("#carouselExample"), {
+        interval: 3000, // Cambia la imagen cada 3 segundos
+        ride: "carousel"
+    });
+
+   
+    
+});
